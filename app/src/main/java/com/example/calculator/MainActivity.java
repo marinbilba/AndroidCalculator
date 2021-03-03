61130ac3269bf0bf2c6785a2c9ae9dbb92a04a7b
Marin Bilba <marynbilba@gmail.com>
2021-03-03 22:00:29 +0100

    Assigned button event listeners

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +12,68 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void zeroButton(View view){
+
+    }
+    public void oneButton(View view){
+
+    }
+    public void twoButton(View view){
+
+    }
+    public void threeButton(View view){
+
+    }
+    public void fourButton(View view){
+
+    }
+    public void fiveButton(View view){
+
+    }
+    public void sixButton(View view){
+
+    }
+    public void sevenButton(View view){
+
+    }
+    public void eightButton(View view){
+
+    }
+    public void nineButton(View view){
+
+    }
+    public void exponentButton(View view){
+
+    }
+    public void subtractButton(View view){
+
+    }
+    public void addButton(View view){
+
+    }
+    public void parenthesesButton(View view){
+
+    }
+    public void equalsButton(View view){
+
+    }
+    public void pointButton(View view){
+
+    }
+    public void plusMinusButton(View view){
+
+    }
+    public void multiplyButton(View view){
+
+    }
+    public void divisionButton(View view){
+
+    }
+    public void clearButton(View view){
+
+    }
+    public void backspaceButton(View view){
+
+    }
+
 }
